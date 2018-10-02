7bb987971052eb0f61ce4ff948b0cea643677f2b
diurno <diurno@pc-aula>
2018-10-02 12:44:48 +0200

    KK

---
+++ KK/src/dgerter/CálculoHipoteca.java
@@ -0,0 +1,18 @@
+package dgerter;
+
+public class CálculoHipoteca {
+
+	public static void main(String[] args) {
+		float euribor = (float) -0.167;
+		float diferencial = (float) 0.89;
+		int capital = 500000;
+		int meses = 120; 
+		float cuotaanual = (float)0.723;
+		
+		System.out.println("euribor + diferencial: " + cuotaanual + "/"); 
+		
+		
+		
+		
+	}
+}
