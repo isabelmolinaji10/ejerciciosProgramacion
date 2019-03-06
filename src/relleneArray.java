b35ecd4a741419d95286664287fdfc5f7fb00035
Isa <diurno@pc-aula>
2019-03-06 10:07:00 +0100

    Rama 2

--- src/relleneArray.java
+++ src/relleneArray.java
@@ -7,6 +7,7 @@ public class relleneArray {
 	
 	public static void relleneArray () {
 		int array [] = new int[10];
+		int sumaElementosIndicePar = 0;
 		// Inicializaci�n del array
 				// Para asignar n�meros al azar entre un l�mite inferior y otro superior
 				// utilizaremos la siguiente f�rmula
@@ -15,10 +16,18 @@ public class relleneArray {
 			array[i] = (int) Math.round(Math.random() * (20 - 1) + 1);
 		}
 		// Impresi�n del array 
-				System.out.println();
-				for (int i = 0; i < array.length; i++) {
-					System.out.print(array[i] + " ");
-				}
+		System.out.println();
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
 		
+	}
+	public static void sumaNumerosIndicePar () {
+		// C�lculo de las sumas de n�meros con �ndice par
+		for (int i = 0; i < array.length; i++) {
+			if ((i & 1) == 0) { // Una forma original de calcular los n�meros con �ndice par
+				sumaElementosIndicePar += array[i];
+			}
+		}		
 	}
 }
